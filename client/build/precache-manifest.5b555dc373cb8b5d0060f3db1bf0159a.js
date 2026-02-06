92ad4250efb5f9216953cdd78e447458f9cc1e53
Qabas <qabasalani1@gmail.com>
2026-02-06 07:33:25 +0700

    added live demo

---
+++ client/build/precache-manifest.5b555dc373cb8b5d0060f3db1bf0159a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1ece622c4764a99ae85c9ae100be58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10801d129e6701104cb9",
+    "url": "/static/css/main.ed16af37.chunk.css"
+  },
+  {
+    "revision": "52132a35b9daf96ee82e",
+    "url": "/static/js/2.e434b6f4.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.e434b6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10801d129e6701104cb9",
+    "url": "/static/js/main.d6405e2a.chunk.js"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
