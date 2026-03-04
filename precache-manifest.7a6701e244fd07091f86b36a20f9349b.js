913a8cd4a28ab8c7d5340eb056592b02e078ca90
Qabas <qabasalani1@gmail.com>
2026-03-04 08:22:36 +0700

    Updates

---
+++ precache-manifest.7a6701e244fd07091f86b36a20f9349b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ad3c03af99b6362377d16edb409c17",
+    "url": "/Redux-Shopping-Store/index.html"
+  },
+  {
+    "revision": "6d7d556adab725f76b4c",
+    "url": "/Redux-Shopping-Store/static/css/main.980df059.chunk.css"
+  },
+  {
+    "revision": "8cf0d84db5be838b08f0",
+    "url": "/Redux-Shopping-Store/static/js/2.bf4ce744.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/Redux-Shopping-Store/static/js/2.bf4ce744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7d556adab725f76b4c",
+    "url": "/Redux-Shopping-Store/static/js/main.df0b7a94.chunk.js"
+  },
+  {
+    "revision": "29c2704892909eb378c6",
+    "url": "/Redux-Shopping-Store/static/js/runtime-main.498f0a92.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/Redux-Shopping-Store/static/media/spinner.ab497855.gif"
+  }
+]);
